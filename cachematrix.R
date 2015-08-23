d4e4ad3304d8b8d9c9b202437380f15982317f4d
illmi <ahmadillmi11@gmail.com>
2015-08-23 16:06:24 +0800

    To obtain the inverse matrix
    Functions like makeCacheMatrix and cacheSolve was use to obtain the
    inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,15 @@
 # makeCacheMatrix creates a list containing a function to
 # set the value of the matrix, get the value of the matrix, set the value of inverse of the matrix, get the value of inverse of the matrix
-
-
-
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 #cacheSolve is used to find the inverse of a matrix, which in this case matrix 'x'
