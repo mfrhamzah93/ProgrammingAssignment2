51cd3185b95cb3d1c4bb01785caed197ee0b7a9b
Icky <icky93@yahoo.com>
2015-08-23 15:00:03 +0800

    To obtain the inverse matrix that were given
    By using functions makeCacheMatrix and cacheSolve, we can make a square
    matrix into inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+# makeCacheMatrix creates a list containing a function to
+# set the value of the matrix, get the value of the matrix, set the value of inverse of the matrix, get the value of inverse of the matrix
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-
-## Write a short comment describing this function
-
+#cacheSolve is used to find the inverse of a matrix, which in this case matrix 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data.")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
 }
+bulu <- makeCacheMatrix( matrix(c(-1,15 1,27), nrow = 2, ncol = 2) )
+summary(bulu)
+cacheSolve(bulu)
+
